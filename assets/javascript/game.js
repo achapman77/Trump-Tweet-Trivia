00ffbdc10b8fb14ceba662b22317c314343030dc
AChapman <achapman77@gmail.com>
2018-11-17 23:19:22 -0700

    Worked on IFs for True and False

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,67 @@
 $(function() {
-//Global Variables
-var score = 0;
-var tweetArray = [
-    {t:"This is tweet 0", a:false},
-    {t:"This is tweet 2", a:true},
-]
+    //Global Variables
+    var score = 0;
+    var tweetArray = [
+        {t:"This is tweet 0", a:true},
+        {t:"This is tweet 2", a:false},
+    ]
 
-function falseButton () {
-    $("#false-button").value(false);
-}
+    var index = 0;
+    var tweet = tweetArray[index].t;
+    var answer = tweetArray[index].a
 
-//Hide Start button on click and show div with trivia, True/False, and timer
-//Also start timer
-$("#big-button").html("<button id='start-button' type='button' class='btn btn-success btn-lg btn-block'>Start</button>")
+    function scoreDisp() {
+        $("#score").text(score + "/20");
+    }
 
-$("#game-board").hide();
+    function nextTweet() {
+        index++;
+        tweet = tweetArray[index].t;
+        answer = tweetArray[index].a
+        $("#tweet").text(tweet);
+        console.log(index);
+        console.log(answer);
+    }
+    //Hide Start button on click and show div with trivia, True/False, and timer
+    //Also start timer
+    $("#big-button").html("<button id='start-button' type='button' class='btn btn-success btn-lg btn-block'>Start</button>")
 
-$("#reset-button").hide();
+    $("#game-board").hide();
 
-$("#big-button").on("click", "#start-button", function(){
-    $("#start-button").hide();
-    $("#game-board").fadeToggle(1750);
-    $("#timer").html("0:00");
-    $("#score").text(score + "/20");
-    $("#question").text(tweetArray[0].t);
-    $("#true-button").val(true);
-    $("#false-button").val(false);
+    $("#reset-button").hide();
 
-});
+    $("#big-button").on("click", "#start-button", function(){
+        $("#start-button").hide();
+        $("#game-board").fadeToggle(1750);
+        $("#true-button").val(true);
+        $("#false-button").val(false);
 
+        $("#timer").html("0:00");
+        scoreDisp();
+    
+        $("#tweet").text(tweet);
+        console.log(answer);
+    });
 
+    $("#true-button").on("click", function(){
+        if (answer === true) {
+            score++;
+            scoreDisp();
+            nextTweet();
+        } else {
+            console.log("Nope!")
+        }
+    })
+
+    $("#false-button").on("click", function(){
+        if (answer === false) {
+            score++;
+            scoreDisp();
+            nextTweet();
+        } else {
+            console.log("Nope Again")
+        }
+    })
 //When user clicks True or False run logic to...
 //Determine If answer is correct, add a point, proceed to next question
 //If answer is incorrect, show correct answer for a couple seconds and proceed to next question
